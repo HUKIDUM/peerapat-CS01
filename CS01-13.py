ec0f69997e0d99e014cfce11d0bb448710e284d2
peerapat wattanakit <Peerapat41437@gmail.com>
2021-07-30 10:52:18 +0700

    Create CS01-13.py

---
+++ CS01-13.py
@@ -0,0 +1,12 @@
+Num = int(input('Enter Your Loop: '))
+NumTotal=[]
+for i in range(Num):
+    data = int(input('Enter your Data: '))
+    NumTotal += [data]
+    print(NumTotal)
+NumTotal.sort(reverse=False)
+print(NumTotal[0])
+nvm = len(NumTotal)
+print(NumTotal[nvm-1])
+
+
